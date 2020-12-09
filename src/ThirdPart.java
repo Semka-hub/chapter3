36123f67e76bd4e3d9673edf7df01005eed110f1
Semyon Golubev <xuevbli@gmail.com>
2020-12-09 03:32:41 +0300

    Shape task complete

--- src/ThirdPart.java
+++ src/ThirdPart.java
@@ -4,21 +4,29 @@ public class ThirdPart {
 
     public static void main(String[] args) {
 
-        /* Написать простое лямба-выражение в переменной myClosure,
-        лямба-выражение должно выводить в консоль фразу "I love Java".
-        Вызвать это лямба-выражение. Далее написать функцию, которая
-        будет запускать заданное лямба-выражение заданное количество
-        раз. Объявить функцию так: public void
-        repeatTask (int times, Runnable task). Функция должна запускать
-        times раз лямба-выражение task . Используйте эту функцию для
-        печати «I love Java 10 раз. */
         /* Лямбда-выражение */
         Printable myClosure = () -> System.out.println("I love Java");
-        repeatTask(10, myClosure::print);
 
+        /* Создаём фигуры для 3-ого задания */
+        Rectangle rectangle = new Rectangle(3, 7);
+        Square square = new Square(5);
+        Circle circle = new Circle(10);
+
+        /* Задание 1 */
+        repeatTask(10, myClosure::print);
 
         /* Задание 2 */
         demonstrateMoving();
+
+        /* Задание 3 */
+        System.out.println("Периметр прямоугольника: " + rectangle.perimeter() + ". " +
+                "Площадь прямоугольника: " + rectangle.area() + ".");
+
+        System.out.println("Периметр квадрата: " + square.perimeter() + ". " +
+                "Площадь квадрата: " + square.area() + ".");
+
+        System.out.println("Периметр круга: " + circle.perimeter() + ". " +
+                "Площадь круга: " + circle.area() + ".");
     }
 
     /* Функция для запуска лямбда-выражения task times раз */
@@ -26,11 +34,6 @@ public class ThirdPart {
         for (int i = 0; i < times; i++) {
             task.run();
         }
-    }
-
-    /* Функциональный интерфейс для лятбда-выражения */
-    interface Printable {
-        void print();
     }
 
     /* Метод, осуществлящий несколько переходов от первоначальной
@@ -74,10 +77,84 @@ public class ThirdPart {
     }
 }
 
+/* Функциональный интерфейс для лятбда-выражения */
+interface Printable {
+    void print();
+}
+
 /* Перечисление возможных направлений движения */
 enum Directions {
     UP,
     DOWN,
     LEFT,
     RIGHT
+}
+
+/* Интерфейс для вывода площади и периметра фигуры */
+interface Shape {
+    double perimeter();
+
+    double area();
+}
+
+/* Класс прямоугольника */
+class Rectangle implements Shape {
+
+    private double width;
+    private double height;
+
+    Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * (width + height);
+    }
+
+    @Override
+    public double area() {
+        return width * height;
+    }
+}
+
+/* Класс квадарата */
+class Square implements Shape {
+
+    private double height;
+
+    Square(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public double perimeter() {
+        return 4 * height;
+    }
+
+    @Override
+    public double area() {
+        return Math.pow(height, 2);
+    }
+}
+
+/* Класс круга */
+class Circle implements Shape {
+
+    private double diameter;
+
+    Circle(double diameter) {
+        this.diameter = diameter;
+    }
+
+    @Override
+    public double perimeter() {
+        return diameter * Math.PI;
+    }
+
+    @Override
+    public double area() {
+        return 0.25 * Math.pow(diameter, 2) * Math.PI;
+    }
 }
